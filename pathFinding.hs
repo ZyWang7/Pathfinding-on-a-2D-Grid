6da87a65b96705beb8c516fa1961cd7c16964169
e31258zw <ziyi.wang-22@student.manchester.ac.uk>
2022-06-22 10:41:12 +0100

    Add function to check the legal positions

--- pathFinding.hs
+++ pathFinding.hs
@@ -11,11 +11,6 @@ grid = ["S.........",
         "..........",
         ".........E"]
 
-grid' = blockPos grid (9,7)
-grid'' = blockPos grid' (8,7)
-grid''' = blockPos grid'' (6,7)
-grid1 = blockPos grid''' (6,8)
-
 
 -- change the value of a list in the given index position to a new value
 setAtIndex :: [a] -> Int -> a -> [a]
@@ -28,4 +23,24 @@ blockPos xs (x,y) | x == 0 = (setAtIndex (head xs) y 'O') : (tail xs)
                   | otherwise = (head xs) : blockPos (tail xs) (x-1,y)
 
 
+grid' = blockPos grid (9,7)
+grid'' = blockPos grid' (8,7)
+grid''' = blockPos grid'' (6,7)
+grid1 = blockPos grid''' (6,8)
+
+
+-- confirm whether the coordinate position (x,y) is legal or not
+posOnGrid :: [[Char]] -> (Int,Int) -> Bool
+posOnGrid vss (x,y) = x >= 0 && x < length(vss)
+                    && y >= 0 && y < length (vss !! x)
+
+-- return the value of item at position (x,y)
+getAtPos :: [[Char]] -> (Int,Int) -> Char
+getAtPos vss (x,y) | posOnGrid vss (x,y) = (vss!!x)!!y
+                   | otherwise = 'O'
+
+-- function to check the position is legal or not
+isLegalPos :: [[Char]] -> (Int,Int) -> Bool
+isLegalPos xs (x,y) = if getAtPos xs (x,y) == 'O' then False
+                    else True
 
